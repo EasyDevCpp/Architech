c21947e02d04913200910df02975f7a820d2c452
EasyDev <easy-dev@web.de>
2018-08-03 18:07:02 +0200

    Updated parser.h license

--- src/compiler/parser.h
+++ src/compiler/parser.h
@@ -1,17 +1,19 @@
 /*
- *	Author: Robin Krause
- *	
- *	E-Mail: easy-dev[at]web.de
+ *	Architech - Yet simple but yet effective programming language
+ *   	Copyright (C) 2018 - Robin Krause
  *
- *	License: GNU Public License v3
+ *   	This program is free software: you can redistribute it and/or modify
+ *   	it under the terms of the GNU General Public License as published by
+ *   	the Free Software Foundation, either version 3 of the License, or
+ *   	(at your option) any later version.
  *
- *	Project:
- *		ArchiTech - Simple yet effictive programming language
+ *   	This program is distributed in the hope that it will be useful,
+ *   	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  	GNU General Public License for more details.
  *
- *	Dependancies:
- *		g++ or any other c++ compiler
- *
- *	Usage: archc input-file output-file [compiler] [c/c++ libaries]
+ *   	You should have received a copy of the GNU General Public License
+ *   	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 std::vector<std::string> remove_tabs(std::vector<std::string> source_file) {
